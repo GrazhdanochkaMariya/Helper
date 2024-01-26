ae8becf72ab5e70afe9ffb3cdc862aca3de07ea3
GrazhdanochkaMariya <mariyashakuro@gmail.com>
2024-01-26 15:05:14 +0300

    2 more tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,22 +1,13 @@
-import base64
-import pdb
-from unittest.mock import MagicMock, patch, AsyncMock
-
 import pytest
 from fastapi import HTTPException
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
-from pytest_mock import mocker
+from fastapi.security import  HTTPBasicCredentials
+
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.requests import Request
-from starlette.status import HTTP_200_OK
-from starlette.testclient import TestClient
 
-from src.admin.auth import SessionAuth
 from src.auth.auth import authenticate_user, create_access_token, swagger_login
 from src.auth.dependencies import get_current_user
-from src.database import async_session_maker
-from src.main import app
-from src.models import User
+
 from tests.conftest import get_user_data
 
 
@@ -40,30 +31,32 @@ async def test_get_current_user(db_session: AsyncSession):
         await get_current_user("invalid_token", db_session)
     assert exc_info.value.status_code == 401
 
-client = TestClient(app)
+
+@pytest.mark.asyncio
+async def test_swagger_login_with_valid_credentials(mocker, db_session: AsyncSession):
+    mock_request = mocker.MagicMock(spec=Request)
+    user_data = get_user_data()
+    credentials = HTTPBasicCredentials(username=user_data['email'], password=user_data['hashed_password'])
+
+    mock_request.headers = {"Authorization": f"Basic {credentials.username}:{credentials.password}"}
+    mock_request.session = {}
+    mocker.patch("src.auth.auth.verify_password", return_value=True)
+
+    await swagger_login(mock_request, credentials=credentials, session=db_session)
+
+    assert "token" in mock_request.session
 
 
-# @pytest.mark.asyncio
-# async def test_swagger_login(db_session: AsyncSession):
-#     credentials = HTTPBasicCredentials(username="masha@gmail.com", password="hashed_password")
-#     response = client.post("/login", headers={"content-type": "application/x-www-form-urlencoded"}, data=credentials.__dict__)
-#     assert response.status_code == 200
+@pytest.mark.asyncio
+async def test_swagger_login_with_invalid_credentials(mocker, db_session: AsyncSession):
+    mock_request = mocker.MagicMock(spec=Request)
+    credentials = HTTPBasicCredentials(username="invalid_name", password="invalid_password")
 
-# @pytest.fixture
-# def mock_request():
-#     return MagicMock(spec=Request)
-#
-#
-# @pytest.mark.asyncio
-# async def test_login_successful(db_session: AsyncSession, mock_request):
-#     form_data = {"username": "masha@gmail.com", "password": "hashed_password"}
-#     mock_request.form = AsyncMock(return_value=form_data)
-#     user_id = 1
-#     authenticate_user.return_value = {"id": user_id}
-#     access_token = "some-access-token"
-#     create_access_token.return_value = access_token
-#
-#     result = await SessionAuth().login(mock_request)
-#
-#     assert result is True
-#     mock_request.session.update.assert_called_once_with({"token": access_token})
+    mock_request.headers = {"Authorization": f"Basic {credentials.username}:{credentials.password}"}
+    mock_request.session = {}
+    mocker.patch("src.auth.auth.verify_password", return_value=True)
+
+    with pytest.raises(HTTPException) as exc_info:
+        await swagger_login(mock_request, credentials=credentials, session=db_session)
+    assert exc_info.value.status_code == 401
+

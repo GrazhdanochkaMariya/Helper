e4cc6f5051f765de37f396f090fd3fa479c0287c
egorvavilov <vavilov.egor.work@gmail.com>
2024-03-25 12:14:00 +0100

    feat: add migrations

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,2 +1,8 @@
+from django.urls import path
 
-urlpatterns = []
+from apps.api.views import ContactAPIView, GoogleSheetsProcessorViews
+
+urlpatterns = [
+    path("check/contact/", ContactAPIView.as_view()),
+    path("gs/changed/", GoogleSheetsProcessorViews.as_view()),
+]

62e83d630e9f519a8d110fd495a462c387bb7967
GrazhdanochkaMariya <mariyashakuro@gmail.com>
2024-03-25 13:32:48 +0300

    Requirements, env_example, models

---
+++ apps/api/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class User(models.Model):
+    email = models.CharField(max_length=255, unique=True)
+    hashed_password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.email
+
+
+class LeadContact(models.Model):
+    class TypeEnum(models.TextChoices):
+        CONTACT = "CONTACT"
+        DNM = "DNM"
+        REQUEST = "REQUEST"
+        DECLINED = "DECLINED"
+
+    lead_name = models.CharField(max_length=50)
+    linkedin_profile = models.CharField(max_length=255, unique=True)
+    status = models.CharField(max_length=50, choices=TypeEnum.choices, default=TypeEnum.CONTACT)
+    next_contact = models.CharField(max_length=50, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.status}: {self.linkedin_profile}"
